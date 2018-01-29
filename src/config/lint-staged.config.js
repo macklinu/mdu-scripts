7ed0e390427bad19de3fc3d1fb3e87f088e91f50
Macklin Underdown <macklinu@users.noreply.github.com>
2018-01-29 11:23:19 -0500

    feat(precommit): format markdown with doctoc and prettier
    Resolves #27

--- src/config/lint-staged.config.js
+++ src/config/lint-staged.config.js
@@ -1,10 +1,13 @@
-const { resolveMduScripts } = require('../utils')
+const { resolveMduScripts, resolveBin } = require('../utils')
 
 const mduScripts = resolveMduScripts()
+const doctoc = resolveBin('doctoc')
 
 module.exports = {
+  concurrent: false,
   linters: {
-    '**/*.+(js|json|ts|tsx|less|css)': [
+    'README.md': [`${doctoc} --maxlevel 3 --notitle`, 'git add'],
+    '**/*.+(js|json|ts|tsx|less|css|md)': [
       `${mduScripts} format`,
       `${mduScripts} lint`,
       'git add',
